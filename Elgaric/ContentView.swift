369d281b5d6a694d16c1900a33574b326fb9f9b4
Dj Walker-Morgan <codepope@gmail.com>
2022-02-11 22:09:07 +0000

    Initial Commit

---
+++ Elgaric/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Elgaric
+//
+//  Created by Dj Walker-Morgan on 11/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Elgaric/ElgaricApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElgaricApp.swift
+//  Elgaric
+//
+//  Created by Dj Walker-Morgan on 11/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ElgaricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
